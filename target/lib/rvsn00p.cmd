27ec9f99ac036b6b2e47b1886bc21c607d2ade57
Örjan Lundberg <orjan.lundberg+github@gmail.com>
2002-10-10 20:51:38 +0000

    Added rvscript.jar in classpath

--- target/lib/rvsn00p.cmd
+++ target/lib/rvsn00p.cmd
@@ -1,4 +1,4 @@
-start javaw -Xincgc -Duse.sdk=false -classpath rvsn00p.jar;c:\tibco\tibrv\lib\tibrvj.jar;c:\tibco\adapter\sdk\java\Maverick4.jar;c:\tibco\im\java\crimson.jar rvsn00p.StartRvSnooper %*
+start javaw -Xincgc -Duse.sdk=false -classpath rvsn00p.jar;c:\tibco\tibrv\lib\tibrvj.jar;c:\tibco\adapter\sdk\java\Maverick4.jar;c:\tibco\im\java\crimson.jar;c:\tibco\rvscript\rvscript.jar rvsn00p.StartRvSnooper %*
 
 
 
