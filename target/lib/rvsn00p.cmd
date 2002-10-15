5c02734ceb9cd32e17d98791183a9138ca8b03b8
Örjan Lundberg <orjan.lundberg+github@gmail.com>
2002-10-15 17:57:06 +0000

    better?

--- target/lib/rvsn00p.cmd
+++ target/lib/rvsn00p.cmd
@@ -1,4 +1,17 @@
-start javaw -Xincgc -Duse.sdk=false -classpath rvsn00p.jar;c:\tibco\tibrv\lib\tibrvj.jar;c:\tibco\adapter\sdk\java\Maverick4.jar;c:\tibco\im\java\crimson.jar;c:\tibco\rvscript\rvscript.jar rvsn00p.StartRvSnooper %*
+@echo off
+TITLE RvSnoop_Start_Script
+set _cp=C:\temp\rvsn00p\target\lib\rvsn00p.jar
+set _cp=%_cp%;c:\tibco\adapter\sdk\java\Maverick4.jar
+set _cp=%_cp%;c:\tibco\im\java\crimson.jar
+set _cp=%_cp%;c:\tibco\rvscript\rvscript.jar
+set _cp=%_cp%;c:\tibco\tibrv\lib\tibrvj.jar
+set _cp=%_cp%;%classpath%
 
+rem start rvsn00p
+rem echo Classpath =  %_cp%
+rem change java to "start javaw" to get rid of the cmd screen
+java -Xincgc -classpath "%_cp%" rvsn00p.StartRvSnooper %*
+echo remove the pause in the start script to get rid of this screen
+pause
 
 
