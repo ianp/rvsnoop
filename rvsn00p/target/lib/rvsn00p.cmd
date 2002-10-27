a7e4912c7b0b88d6a86c4467f8ea2f354fcf6eba
Örjan Lundberg <orjan.lundberg+github@gmail.com>
2002-10-27 21:48:53 +0000

    1.1.9 Changes up to version 1.1.9 Sun Oct 27 21:28:46 CET 2002Added: 629013 Save table/message to rvscript file Display of java "file.encoding" in status bar Added -Dfile.encoding=UTF8 in startup script Added ico file that you can select as an icon in windows when you create a shortcut

--- rvsn00p/target/lib/rvsn00p.cmd
+++ rvsn00p/target/lib/rvsn00p.cmd
@@ -3,14 +3,19 @@ TITLE RvSnoop_Start_Script
 set _cp=C:\temp\rvsn00p\target\lib\rvsn00p.jar
 set _cp=%_cp%;c:\tibco\adapter\sdk\java\Maverick4.jar
 set _cp=%_cp%;c:\tibco\im\java\crimson.jar
-set _cp=%_cp%;c:\tibco\rvscript\rvscript.jar
+rem set _cp=%_cp%;c:\tibco\rvscript\rvscript.jar
 set _cp=%_cp%;c:\tibco\tibrv\lib\tibrvj.jar
 set _cp=%_cp%;%classpath%
 
+set l1="tcp:7500|7500||>"
+set alllisteners=%l1%
+
 rem start rvsn00p
 rem echo Classpath =  %_cp%
-rem change java to "start javaw" to get rid of the cmd screen
-java -Xincgc -classpath "%_cp%" rvsn00p.StartRvSnooper %*
+
+rem to change the encoding character set change the -Dfile.encoding parameters
+rem see http://java.sun.com/j2se/1.3/docs/guide/intl/encoding.doc.html
+java -Xincgc -Dfile.encoding=UTF8 -classpath "%_cp%" rvsn00p.StartRvSnooper %l1% %*
 echo remove the pause in the start script to get rid of this screen
 pause
 
--- rvsn00p/target/lib/rvsn00p.sh
+++ rvsn00p/target/lib/rvsn00p.sh
@@ -1,3 +1,3 @@
 #!/bin/ksh
-java -classpath rvsn00p.jar:/usr/local/tibco/tibrv/lib/tibrvj.jar:/usr/local/tibco/adapter/sdk/java/Maverick4.jar:/usr/local/tibco/im/java/crimson.jar:$CLASSPATH rvsn00p.StartRvSnooper $* &
+java -classpath rvsn00p.jar:/usr/local/tibco/tibrv/lib/tibrvj.jar:/usr/local/tibco/adapter/sdk/java/Maverick4.jar:/usr/local/tibco/im/java/crimson.jar:$CLASSPATH -Dfile.encoding=UTF8 rvsn00p.StartRvSnooper $* &
 
