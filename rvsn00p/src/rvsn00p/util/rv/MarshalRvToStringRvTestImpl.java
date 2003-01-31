a5e9273bfa6211216206e163b8c2cceae7e3bb15
Örjan Lundberg <orjan.lundberg+github@gmail.com>
2003-01-31 20:45:33 +0000

    Added rvtest serializer

---
+++ rvsn00p/src/rvsn00p/util/rv/MarshalRvToStringRvTestImpl.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software
+ * License version 1.1, a copy of which has been included with this
+ * distribution in the LICENSE.txt file.
+ */
+package rvsn00p.util.rv;
+
+import com.tibco.tibrv.TibrvMsg;
+import com.reuters.msgtest.XMLConverter ;
+
+
+/***
+ * The conversion utility between a TibrvMsg and an XML String for rvsn00p
+ * <p>
+ * The XML String is rather verbose because it works with any kind of TIB/Rendezvous message, either a TibrvMsg or
+ * a MTree or a MInstance.
+ *
+ * @author <a href="mailto:crouvrais@users.sourceforge.net">Cedric ROUVRAIS</a>
+ * @version $Id$
+ * @since Raccoon v0.5. Created 28 janv. 2003 at 20:45:22
+ */
+public class MarshalRvToStringRvTestImpl implements IMarshalRvToStringImpl {
+    /*** The TIB/Rendezvous message to XMLConverter */
+    private XMLConverter xc = new XMLConverter();
+    /***
+     * Converts a TibrvMsg into an xml string
+     * @param rvMsg the TIB/Rendezvous message
+     * @param name
+     * @return the XML String
+     */
+    public String rvmsgToString(TibrvMsg rvMsg, String name) {
+        try {
+            StringBuffer sb;
+            sb =  xc.createXML(rvMsg);
+            return sb.toString();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /***
+     * Converts an XML String into a TibrvMsg.
+     *
+     * @throws Exception "Not implemented"
+     */
+    public TibrvMsg stringToRvmsg(String pXmlString) throws Exception {
+        throw new Exception("Not implemented");
+    }
+
+}
