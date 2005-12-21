3a06c9f8c0ffb1fab14d2d126f60f5da85fce58a
Ian Phillips <ianp@ianp.org>
2005-12-21 18:54:42 +0000

    Added treeonly and text only switches, updated arg parseing code.

--- src/rvsn00p/StartRvSnooper.java
+++ src/rvsn00p/StartRvSnooper.java
@@ -1,103 +1,86 @@
-/*
- * Copyright (C) The Apache Software Foundation. All rights reserved.
- *
- * This software is published under the terms of the Apache Software
- * License version 1.1, a copy of which has been included with this
- * distribution in the LICENSE.txt file.
- */
+//:File:    StartRvSnooper.java
+//:Legal:   Copyright © 2002-@year@ Apache Software Foundation.
+//:Legal:   Copyright © 2005-@year@ Ian Phillips.
+//:License: Licensed under the Apache License, Version 2.0.
+//:CVSID:   $Id$
 package rvsn00p;
 
-import rvsn00p.util.rv.RvParameters;
-import rvsn00p.viewer.RvSnooperGUI;
-
-import java.awt.*;
+import java.awt.Toolkit;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
 
+import rvsn00p.util.rv.RvParameters;
+import rvsn00p.viewer.RvSnooperGUI;
+
 /**
  * Starts an instance of the RvSn00p console for off-line viewing.
+ * <p>
+ * Based on <a href="http://wiki.apache.org/logging-log4j/LogFactor5">Log Factor 5</a>.
  *
- * @author �rjan Lundberg
- * Based on work done on Logfactor5 Contributed by ThoughtWorks Inc. by
- * @author Brad Marlborough
- * @author Richard Hurst
+ * @author <a href="mailto:lundberg@home.se">Örjan Lundberg</a>
+ * @author <a href="mailto:ianp@ianp.org">Ian Phillips</a>
+ * @version $Revision$, $Date$
  */
-
-
 public class StartRvSnooper {
-    //--------------------------------------------------------------------------
-    //   Constants:
-    //--------------------------------------------------------------------------
-
-    //--------------------------------------------------------------------------
-    //   Protected Variables:
-    //--------------------------------------------------------------------------
-
-    //--------------------------------------------------------------------------
-    //   Private Variables:
-    //--------------------------------------------------------------------------
-
-    //--------------------------------------------------------------------------
-    //   Constructors:
-    //--------------------------------------------------------------------------
-
-    //--------------------------------------------------------------------------
-    //   Public Methods:
-    //--------------------------------------------------------------------------
 
     /**
      * Main - starts a an instance of the RvSnoop console and configures
      * the console settings.
      */
     public final static void main(String[] args) {
+        args = args != null ? args : new String[0];
         int startAt = 0;
-        String title;
-        title = null;
+        String title = null;
+        boolean text = true, tree = true;
 
-
-        if (args.length != 0) {
-            if (args[0].compareToIgnoreCase("-h") == 0) {
-                System.err.print(RvSnooperGUI.VERSION);
-                System.err.println(" " + System.getProperty("java.vm.name") + " " + System.getProperty("java.vm.version"));
-                System.err.println("Usage: rvsnoop.StartRvSnooper [-title t] [Daemon|Service|Network|Subject] ...  ");
-                System.err.println("Example: rvsnoop.StartRvSnooper \"tcp:7500|7500||a.>,c.x\" \"tcp:7500|7501||b.>,q.b\"  ");
-                System.exit(-1);
-            } else if (args[0].compareToIgnoreCase("-title") == 0) {
-                title = args[1];
-                startAt = 2;
+        while (startAt < args.length && args[startAt].charAt(0) == '-') {
+            String arg = args[startAt++].substring(1);
+            if ("h".equals(arg)) {
+                displayUsageAndExit();
+            } else if ("title".equals(arg)) {
+                title = args[startAt++];
+            } else if ("textonly".equals(arg)) {
+                tree = false;
+            } else if ("treeonly".equals(arg)) {
+                text = false;
             }
         }
-
+        if (!text && !tree)
+            displayUsageAndExit();
+        if (checkJavaVersion() == false)
+            System.err.println("Warning: Java JRE Version 1.4.1 or higher is required");
 
         Set setRvListenersParam = new HashSet();
         if (args.length > 0) {
-            for (int iarg = startAt; args.length > iarg; ++iarg) {
-                RvParameters p = new RvParameters();
-                p.configureByLineString(args[iarg]);
-                setRvListenersParam.add(p);
-            }
-
+            for (int iarg = startAt; args.length > iarg; ++iarg)
+                setRvListenersParam.add(RvParameters.parseConfigurationString(args[iarg]));
             System.out.print(RvSnooperGUI.VERSION);
             System.out.print(" on " + System.getProperty("java.vm.name") + " " + System.getProperty("java.vm.version"));
             System.out.print(" " + System.getProperty("os.name") + " " + System.getProperty("os.arch"));
             System.out.println(" " + System.getProperty("os.version"));
         }
-
-        if (checkJavaVersion() == false) {
-            System.err.println("Warning: Java JRE Version 1.4.1 or higher is required");
-        }
-
         RvSnooperGUI gui = new RvSnooperGUI(
-                MsgType.getAllDefaultLevels(), setRvListenersParam, title);
-
+                MsgType.getAllDefaultLevels(), setRvListenersParam, title, tree, text);
         gui.show();
 
     }
 
-    //--------------------------------------------------------------------------
-    //   Protected Methods:
-    //--------------------------------------------------------------------------
+    private static void displayUsageAndExit() {
+        System.out.print(RvSnooperGUI.VERSION);
+        System.out.println("Usage: java rvsn00p.StartRvSnooper [-title <t>] [-{textonly|treeonly}] [<daemon>|<service>|<network>|<subjects>] ...");
+        System.out.println("");
+        System.out.println("  -title    : set the window title to <title>.");
+        System.out.println("  -treeonly : show only the tree view of message details (new-style).");
+        System.out.println("  -textonly : show only the text view of message details (old-style).");
+        System.out.println("  <service> : the Rendezvous service parameter.");
+        System.out.println("  <network> : the Rendezvous network parameter.");
+        System.out.println("  <daemon>  : the Rendezvous daemon parameter.");
+        System.out.println("  <subjects>: a comma-separated list of Rendezvous subject patterns.");
+        System.out.println("");
+        System.out.println("Example: java rvsn00p.StartRvSnooper \"tcp:7500|7500||a.>,c.x\" \"tcp:7500|7501||b.>,q.b\"");
+        System.exit(-1);
+    }
 
     protected static int getDefaultMonitorWidth() {
         return (3 * getScreenWidth()) / 4;
@@ -134,7 +117,6 @@ public class StartRvSnooper {
     }
 
     /**
-     *
      * @return boolean is java version ok
      */
     protected static boolean checkJavaVersion() {
@@ -154,8 +136,7 @@ public class StartRvSnooper {
             retVal = (a >= 1 && b >= 4);
             if (a == 1 && b == 4) {
                 if (st.hasMoreTokens() == true) {
-                    int c;
-                    c = Integer.parseInt(st.nextToken());
+                    Integer.parseInt(st.nextToken());
                     if (Integer.parseInt(st.nextToken()) >= 1) {
                         retVal = true;
                     } else {
@@ -173,14 +154,5 @@ public class StartRvSnooper {
         return retVal;
 
     }
-    //--------------------------------------------------------------------------
-    //   Private Methods:
-    //--------------------------------------------------------------------------
-
-    //--------------------------------------------------------------------------
-    //   Nested Top-Level Classes or Interfaces
-    //--------------------------------------------------------------------------
 
 }
-
-
