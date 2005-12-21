0e012ea6d1d28b2bd06e02170ea463348474f88b
Ian Phillips <ianp@ianp.org>
2005-12-21 19:02:28 +0000

    Code clean-up.

--- src/rvsn00p/util/rv/MarshalRvToStringRvMsgImpl.java
+++ src/rvsn00p/util/rv/MarshalRvToStringRvMsgImpl.java
@@ -1,18 +1,16 @@
-/*
- * Copyright (C) The Apache Software Foundation. All rights reserved.
- *
- * This software is published under the terms of the Apache Software
- * License version 1.1, a copy of which has been included with this
- * distribution in the LICENSE.txt file.
- */
-
+//:File:    MarshalRvToStringRvMsgImpl.java
+//:Legal:   Copyright © 2002-@year@ Apache Software Foundation.
+//:Legal:   Copyright © 2005-@year@ Ian Phillips.
+//:License: Licensed under the Apache License, Version 2.0.
+//:CVSID:   $Id$
 package rvsn00p.util.rv;
-import  com.tibco.tibrv.TibrvMsg;
-import  com.tibco.tibrv.TibrvCmMsg;
+
+import com.tibco.tibrv.TibrvMsg;
 
 /**
- *  MarshalRvToString impementation with RvMsg
- * @author �rjan Lundberg
+ * MarshalRvToString impementation with RvMsg
+ *
+ * @author <a href="mailto:lundberg@home.se">Örjan Lundberg</a>
  */
 class MarshalRvToStringRvMsgImpl implements IMarshalRvToStringImpl {
     public MarshalRvToStringRvMsgImpl() {
--- src/rvsn00p/util/rv/MarshalRvToStringRvTestImpl.java
+++ src/rvsn00p/util/rv/MarshalRvToStringRvTestImpl.java
@@ -1,15 +1,12 @@
-/*
- * Copyright (C) The Apache Software Foundation. All rights reserved.
- *
- * This software is published under the terms of the Apache Software
- * License version 1.1, a copy of which has been included with this
- * distribution in the LICENSE.txt file.
- */
+//:File:    MarshalRvToStringRvTestImpl.java
+//:Legal:   Copyright © 2002-@year@ Apache Software Foundation.
+//:Legal:   Copyright © 2005-@year@ Ian Phillips.
+//:License: Licensed under the Apache License, Version 2.0.
+//:CVSID:   $Id$
 package rvsn00p.util.rv;
 
+import com.reuters.msgtest.XMLConverter;
 import com.tibco.tibrv.TibrvMsg;
-import com.reuters.msgtest.XMLConverter ;
-
 
 /***
  * Rvtest plugin - string serialiser from TibrvMsg to the rvtest XML format for rvsn00p
@@ -17,9 +14,10 @@ import com.reuters.msgtest.XMLConverter ;
  * The XML String is rather verbose because it works with any kind of TIB/Rendezvous message, either a TibrvMsg or
  * a MTree or a MInstance.
  *
- * @author <a href="mailto:lundberg@home.se">Oran Lundberg</a>
- * @version $Id$
- * @since Rvsn00p v1.2.6
+ * @author <a href="mailto:lundberg@home.se">Örjan Lundberg</a>
+ * @author <a href="mailto:ianp@ianp.org">Ian Phillips</a>
+ * @version $Revision$, $Date$
+ * @since 1.2.6
  */
 public class MarshalRvToStringRvTestImpl implements IMarshalRvToStringImpl {
     /*** The TIB/Rendezvous message to XMLConverter */
