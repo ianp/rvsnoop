9f646a88e0b6e17c7cf94b3bd61f4bd203dae832
Ian Phillips <ianp@ianp.org>
2005-12-23 11:01:29 +0000

    Removed unused methods.

--- rvsn00p/src/rvsn00p/StartRvSnooper.java
+++ rvsn00p/src/rvsn00p/StartRvSnooper.java
@@ -5,7 +5,6 @@
 //:CVSID:   $Id$
 package rvsn00p;
 
-import java.awt.Toolkit;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
@@ -14,19 +13,60 @@ import rvsn00p.util.rv.RvParameters;
 import rvsn00p.viewer.RvSnooperGUI;
 
 /**
- * Starts an instance of the RvSn00p console for off-line viewing.
+ * Starts an instance of RvSn00p.
  * <p>
- * Based on <a href="http://wiki.apache.org/logging-log4j/LogFactor5">Log Factor 5</a>.
- *
+ * This is just a collection of bootstrap methods to ensure that the environment
+ * is suitable and parse the command line arguments.
+ * <p>
+ * Based on <a href="http://wiki.apache.org/logging-log4j/LogFactor5">Log Factor
+ * 5</a>.
+ * 
  * @author <a href="mailto:lundberg@home.se">Örjan Lundberg</a>
  * @author <a href="mailto:ianp@ianp.org">Ian Phillips</a>
  * @version $Revision$, $Date$
  */
-public class StartRvSnooper {
+public final class StartRvSnooper {
+
+    private static void displayUsageAndExit() {
+        System.out.print(RvSnooperGUI.VERSION);
+        System.out.println("Usage: java rvsn00p.StartRvSnooper [-title <t>] [-{textonly|treeonly}] [<daemon>|<service>|<network>|<subjects>] ...");
+        System.out.println("");
+        System.out.println("  -title    : set the window title to <title>.");
+        System.out.println("  -treeonly : show only the tree view of message details (new-style).");
+        System.out.println("  -textonly : show only the text view of message details (old-style).");
+        System.out.println("  <service> : the Rendezvous service parameter.");
+        System.out.println("  <network> : the Rendezvous network parameter.");
+        System.out.println("  <daemon>  : the Rendezvous daemon parameter.");
+        System.out.println("  <subjects>: a comma-separated list of Rendezvous subject patterns.");
+        System.out.println("");
+        System.out.println("Example: java rvsn00p.StartRvSnooper \"tcp:7500|7500||a.>,c.x\" \"tcp:7500|7501||b.>,q.b\"");
+        System.exit(-1);
+    }
 
     /**
-     * Main - starts a an instance of the RvSnoop console and configures
-     * the console settings.
+     * Checks whether the JVM version is suitable.
+     * 
+     * @return <code>true</code> if the JVM is OK, <code>false</code> otherwise.
+     */
+    private static boolean isCorrectJavaVersion() {
+        String ver = System.getProperty("java.version");
+        try {
+            StringTokenizer st = new StringTokenizer(ver, "._-");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (a > 1) return true;
+            if (b > 4) return true;
+            return st.hasMoreTokens() && Integer.parseInt(st.nextToken()) > 1;
+        } catch(Exception e) {
+            System.err.println("Warning: unable to determine Java version.");
+            return true;
+        }
+    }
+
+    /**
+     * The application entry point.
+     * 
+     * @param args The command line arguments.
      */
     public final static void main(String[] args) {
         args = args != null ? args : new String[0];
@@ -46,10 +86,11 @@ public class StartRvSnooper {
                 text = false;
             }
         }
+
         if (!text && !tree)
             displayUsageAndExit();
-        if (checkJavaVersion() == false)
-            System.err.println("Warning: Java JRE Version 1.4.1 or higher is required");
+        if (!isCorrectJavaVersion())
+            System.err.println("Warning: Java version 1.4.2 or higher is required");
 
         Set setRvListenersParam = new HashSet();
         if (args.length > 0) {
@@ -63,95 +104,6 @@ public class StartRvSnooper {
         RvSnooperGUI gui = new RvSnooperGUI(
                 MsgType.getAllDefaultLevels(), setRvListenersParam, title, tree, text);
         gui.show();
-
-    }
-
-    private static void displayUsageAndExit() {
-        System.out.print(RvSnooperGUI.VERSION);
-        System.out.println("Usage: java rvsn00p.StartRvSnooper [-title <t>] [-{textonly|treeonly}] [<daemon>|<service>|<network>|<subjects>] ...");
-        System.out.println("");
-        System.out.println("  -title    : set the window title to <title>.");
-        System.out.println("  -treeonly : show only the tree view of message details (new-style).");
-        System.out.println("  -textonly : show only the text view of message details (old-style).");
-        System.out.println("  <service> : the Rendezvous service parameter.");
-        System.out.println("  <network> : the Rendezvous network parameter.");
-        System.out.println("  <daemon>  : the Rendezvous daemon parameter.");
-        System.out.println("  <subjects>: a comma-separated list of Rendezvous subject patterns.");
-        System.out.println("");
-        System.out.println("Example: java rvsn00p.StartRvSnooper \"tcp:7500|7500||a.>,c.x\" \"tcp:7500|7501||b.>,q.b\"");
-        System.exit(-1);
-    }
-
-    protected static int getDefaultMonitorWidth() {
-        return (3 * getScreenWidth()) / 4;
-    }
-
-    protected static int getDefaultMonitorHeight() {
-        return (3 * getScreenHeight()) / 4;
-    }
-
-    /**
-     * @return the screen width from Toolkit.getScreenSize()
-     * if possible, otherwise returns 800
-     * @see java.awt.Toolkit
-     */
-    protected static int getScreenWidth() {
-        try {
-            return Toolkit.getDefaultToolkit().getScreenSize().width;
-        } catch (Throwable t) {
-            return 800;
-        }
-    }
-
-    /**
-     * @return the screen height from Toolkit.getScreenSize()
-     * if possible, otherwise returns 600
-     * @see java.awt.Toolkit
-     */
-    protected static int getScreenHeight() {
-        try {
-            return Toolkit.getDefaultToolkit().getScreenSize().height;
-        } catch (Throwable t) {
-            return 600;
-        }
-    }
-
-    /**
-     * @return boolean is java version ok
-     */
-    protected static boolean checkJavaVersion() {
-
-        boolean retVal;
-        String ver;
-        ver = System.getProperty("java.version");
-        retVal = false;
-        try {
-            StringTokenizer st = new StringTokenizer(ver, "._-");
-            int a,b;
-            a = b = -1;
-
-            a = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
-
-            retVal = (a >= 1 && b >= 4);
-            if (a == 1 && b == 4) {
-                if (st.hasMoreTokens() == true) {
-                    Integer.parseInt(st.nextToken());
-                    if (Integer.parseInt(st.nextToken()) >= 1) {
-                        retVal = true;
-                    } else {
-                        retVal = false;
-                    }
-                } else {
-                    retVal = false;
-                }
-
-            }
-        }catch(Exception ex){
-            retVal = true;
-        }
-
-        return retVal;
 
     }
 
