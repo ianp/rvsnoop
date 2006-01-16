27cabbab4d5f6f3798b869fe09cfb98a1e0cb07d
Ian Phillips <ianp@ianp.org>
2006-01-16 17:32:22 +0000

    Added JavaDoc for all fields.

--- rvsn00p/src/rvsnoop/ui/Banners.java
+++ rvsn00p/src/rvsnoop/ui/Banners.java
@@ -20,13 +20,39 @@ public final class Banners {
 
     private static final int ICON_SIZE = 48;
 
+    /**
+     * Standard message banner for details panel.
+     */
     public static final Icon MESSAGE = Icons.createIcon("/resources/banners/message.png", ICON_SIZE);
+    /**
+     * Advisory message banner for details panel.
+     */
     public static final Icon MESSAGE_ADVISORY = Icons.createIcon("/resources/banners/message_advisory.png", ICON_SIZE);
+    /**
+     * Certified messaging message banner for details panel.
+     */
     public static final Icon MESSAGE_CM = Icons.createIcon("/resources/banners/message_rvcm.png", ICON_SIZE);
+    /**
+     * Default empty banner for details panel.
+     */
     public static final Icon MESSAGE_NONE = Icons.createIcon("/resources/banners/message_none.png", ICON_SIZE);
+    /**
+     * Fault tolerant message banner for details panel.
+     */
     public static final Icon MESSAGE_FT = Icons.createIcon("/resources/banners/message_rvft.png", ICON_SIZE);
+    /**
+     * Bannder for confirm quit dialog.
+     */
     public static final Icon QUIT = Icons.createIcon("/resources/banners/quit.png", ICON_SIZE);
+    /**
+     * Banner for update check dialog.
+     * This image is used when no update is available.
+     */
     public static final Icon UPDATE_ALREADY = Icons.createIcon("/resources/banners/update_already.png", ICON_SIZE);
+    /**
+     * Banner for update check dialog.
+     * This image is used when a new update is available.
+     */
     public static final Icon UPDATE_AVAILABLE = Icons.createIcon("/resources/banners/update_available.png", ICON_SIZE);
     
     /**
