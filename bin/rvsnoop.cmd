1a65be9604db39e4e99003309b7809d266cd6ce8
Ian Phillips <ianp@ianp.org>
2006-01-16 17:14:32 +0000

    Adding wildcarded jars now works.

--- bin/rvsnoop.cmd
+++ bin/rvsnoop.cmd
@@ -1,8 +1,22 @@
 @echo off
-rem Start script for RvSnoop @version@ on Windows platforms.
+rem Start script for rvSnoop @version@ on Windows platforms.
 rem $Id$
 
-title RvSnoop @version@
+setlocal
+
+title rvSnoop @version@
+
+goto :skip_function_defs
+
+:add_to_cp_head
+  set CP=%1;%CP%
+  goto :eof
+
+:add_to_cp_tail
+  set CP=%CP%;%1
+  goto :eof
+
+:skip_function_defs
 
 set RVSNOOP_HOME=%~dp0%..
 
@@ -13,16 +27,18 @@ if "no%$TIBRV_HOME%" equ "no" set TIBRV_HOME=%TIBCO_HOME%\tibrv
 
 set PATH=%TIBRV_HOME%\bin;%PATH%
 
-for /r %RVSNOOP_HOME%\lib %%A in (*.jar) do set CP=%%A;%CP%
+for /r %RVSNOOP_HOME%\lib %%A in (*.jar) do (call :add_to_cp_head %%A)
 
 rem Look for optional libraries to add to the classpath.
 if "no%TIBCO_TRA_HOME%" neq "no" set CP=%CP%;%TIBCO_TRA_HOME%\lib\TIBCOrt.jar
 if "no%TIBSDK_HOME%"    neq "no" set CP=%CP%;%TIBSDK_HOME%\lib\Maverick4.jar
 if "no%RVSCRIPT_HOME%"  neq "no" set CP=%CP%;%RVSCRIPT_HOME%\rvscript.jar
-if "no%RVTEST_HOME%"    neq "no" for /r %RVTEST_HOME%\lib %%A in (*.jar) do set CP=%CP%;%%A
+if "no%RVTEST_HOME%"    neq "no" for /r %RVTEST_HOME%\lib %%A in (*.jar) do (call :add_to_cp_tail %%A)
 
 rem Add the default classpath if present.
 if "no%CLASSPATH%" neq "no" set CP=%CP%;%CLASSPATH%
 
 rem Change the "start javaw" on the next line to "java" to see a console log of any errors.
 start javaw -Xmx128m -Drvsnoop.home=%RVSNOOP_HOME% -Dfile.encoding=UTF-8 -classpath "%CP%" rvsnoop.StartRvSnooper %*
+
+endlocal
