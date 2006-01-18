f082b447184de97048c5397cff44c3a78074d885
Ian Phillips <ianp@ianp.org>
2006-01-18 18:00:13 +0000

    Refactored common code from open/save actions into separate class.

---
+++ src/rvsnoop/ProjectFileFilter.java
@@ -0,0 +1,36 @@
+//:File:    ProjectFileFilter.java
+//:Created: Jan 17, 2006
+//:Legal:   Copyright © 2006-@year@ Apache Software Foundation.
+//:Legal:   Copyright © 2006-@year@ Ian Phillips.
+//:License: Licensed under the Apache License, Version 2.0.
+//:CVSID:   $Id$
+package rvsnoop;
+
+import javax.swing.filechooser.FileFilter;
+import java.io.File;
+import java.util.Locale;
+
+/**
+ * Filter for project files.
+ *
+ * @author <a href="mailto:ianp@ianp.org">Ian Phillips</a>
+ * @version $Revision$, $Date$
+ * @since 1.5
+ */
+// Class provides static instance instead of factory method.
+// @PMD:REVIEWED:MissingStaticMethodInNonInstantiatableClass: by ianp on 1/17/06 2:00 PM
+public class ProjectFileFilter extends FileFilter {
+    public static final ProjectFileFilter INSTANCE = new ProjectFileFilter();
+    
+    private ProjectFileFilter() {
+        super();
+    }
+
+    public boolean accept(File f) {
+        return f.isDirectory() || f.getName().toLowerCase(Locale.ENGLISH).endsWith(".rsp");
+    }
+
+    public String getDescription() {
+        return "rvSnoop Project Files";
+    }
+}
