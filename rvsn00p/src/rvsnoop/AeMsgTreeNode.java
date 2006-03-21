c4db1f3bc1d25fc0071a47fc8b82a84faca59de1
Ian Phillips <ianp@ianp.org>
2006-03-21 13:27:22 +0000

    Altered capitalization of label strings. Renamed "label" field to "text".

--- rvsn00p/src/rvsnoop/AeMsgTreeNode.java
+++ rvsn00p/src/rvsnoop/AeMsgTreeNode.java
@@ -6,15 +6,16 @@
 //:CVSID:   $Id$
 package rvsnoop;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.*;
+
 import com.tibco.sdk.MMessageFormat;
 import com.tibco.sdk.MTree;
 import com.tibco.tibrv.TibrvException;
 import com.tibco.tibrv.TibrvMsg;
 import rvsnoop.ui.Icons;
-
-import javax.swing.Icon;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * A {@link javax.swing.tree.TreeNode TreeNode} that wraps an Active Enterprise
@@ -23,7 +24,7 @@ import java.util.List;
  * At the moment this is a bit of a hack, it should really use a repository
  * connection to create {@link com.tibco.sdk.metadata.MInstance MInstance}s and
  * work with those.
- * 
+ *
  * @author <a href="mailto:ianp@ianp.org">Ian Phillips</a>
  * @version $Revision$, $Date$
  * @since 1.5
@@ -33,29 +34,29 @@ public final class AeMsgTreeNode extends LazyTreeNode {
     private static final Logger logger = Logger.getLogger(AeMsgTreeNode.class);
 
     private final List children = new ArrayList(2);
-    
-    private final String label;
-    
+
+    private final String text;
+
     public AeMsgTreeNode(TibrvMsg message) throws TibrvException {
         super(null);
         MTree data = new MTree("");
         data.use_tibrvMsg(message);
         switch (data.getMessageFormat()) {
         case MMessageFormat.AERV :
-            label = "AE Message (format: RV, version: " + message.getField("^ver^").data + ")";
-            children.add(new RvFieldTreeNode(this, message.getField("^data^")));
+            text = "Active Enterprise Message (Format: RV, Version: " + message.getField("^ver^").data + ")";
+            children.add(new RvFieldTreeNode(this, message.getField("^data^"), "Message Data", null));
             break;
         case MMessageFormat.XMLRV :
-            label = "AE Message (format: XML, version: " + message.getField("^ver^").data + ")";
+            text = "Active Enterprise Message (Format: XML, Version: " + message.getField("^ver^").data + ")";
             // final TibrvMsg m = (TibrvMsg) message.getField("^data^").data;
-            children.add(new RvFieldTreeNode(this, message.getField("^xmldata^")));
+            children.add(new RvFieldTreeNode(this, message.getField("^xmldata^"), "Message Data", null));
             break;
         default:
             throw new IllegalArgumentException("Unsupported message format: " + data.getMessageFormat());
         }
-        children.add(new RvFieldTreeNode(this, message.getField("^tracking^")));
+        children.add(new RvFieldTreeNode(this, message.getField("^tracking^"), "Tracking Information", Icons.TRACKING));
     }
-    
+
 
     /* (non-Javadoc)
      * @see rvsnoop.LazyTreeNode#createChildren()
@@ -79,7 +80,7 @@ public final class AeMsgTreeNode extends LazyTreeNode {
      * @see rvsnoop.LazyTreeNode#getText()
      */
     public String getText() {
-        return label;
+        return text;
     }
 
 }
