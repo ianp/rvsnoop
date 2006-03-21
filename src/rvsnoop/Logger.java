176efa32b127caba5c5959460ff424b1377aad21
Ian Phillips <ianp@ianp.org>
2006-03-21 13:30:08 +0000

    Log level is no longer final. Added methods for changing log level at runtime.

--- src/rvsnoop/Logger.java
+++ src/rvsnoop/Logger.java
@@ -6,9 +6,6 @@
 //:CVSID:   $Id$
 package rvsnoop;
 
-import rvsnoop.ui.UIManager;
-import rvsnoop.ui.UIUtils;
-
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -16,6 +13,9 @@ import java.io.PrintWriter;
 import java.io.Writer;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import rvsnoop.ui.UIManager;
+import rvsnoop.ui.UIUtils;
 
 /**
  * A trivial logging facility.
@@ -25,26 +25,11 @@ import java.util.Date;
  * it has a few warts as well, and is a big extra dependency). This class is
  * basically here to let me defer the decision for a while whilst writing
  * logging into the application.
- * 
+ *
  * @author <a href="mailto:ianp@ianp.org">Ian Phillips</a>
  * @version $Revision$, $Date$
  */
 public final class Logger {
-
-    private static class NullWriter extends Writer {
-        NullWriter() {
-            super();
-        }
-        public void close() throws IOException {
-            // Do nothing.
-        }
-        public void flush() throws IOException {
-            // Do nothing.
-        }
-        public void write(char[] cbuf, int off, int len) throws IOException {
-            // Do nothing.
-        }
-    }
 
     private static final int DEBUG = 5;
     private static final int INFO  = 4;
@@ -52,12 +37,8 @@ public final class Logger {
     private static final int ERROR = 2;
     private static final int FATAL = 1;
     private static final int OFF   = 0;
-    
-    private static final String DEBUG_LABEL = "DEBUG";
-    private static final String INFO_LABEL  = "INFO";
-    private static final String WARN_LABEL  = "WARN";
-    private static final String ERROR_LABEL = "ERROR";
-    private static final String FATAL_LABEL = "FATAL";
+
+    private static final String[] LABELS = { "OFF", "FATAL", "ERROR", "WARN", "INFO", "DEBUG" };
 
     private static final String CONFIG_DIRECTORY = ".rvsnoop";
 
@@ -66,50 +47,38 @@ public final class Logger {
     private static final Date date = new Date();
 
     private static boolean isRunningHeadless = true;
-    
-    private static final int level;
-    
-    private static final Writer writer;
-    
+
+    // Be moderately verbose by default.
+    private static int level = INFO;
+
+    private static Writer writer;
+
     static {
-        final String s = System.getProperty("rvsnoop.logging");
-        if ("debug".equals(s))
-            level = DEBUG;
-        else if ("info".equals(s))
-            level = INFO;
-        else if ("warn".equals(s))
-            level = WARN;
-        else if ("error".equals(s))
-            level = ERROR;
-        else if ("fatal".equals(s))
-            level = FATAL;
-        else if ("off".equals(s))
-            level = OFF;
-        else
-            level = INFO;
-        if (level == OFF) {
-            writer = new NullWriter();
-        } else {
-            final String home = System.getProperty("user.home");
-            final String fs = System.getProperty("file.separator");
-            final String filename = "log-" + new SimpleDateFormat("yyyyMMddHH").format(new Date());
-            try {
-                final File file = new File(home + fs + CONFIG_DIRECTORY + fs + filename);
-                file.getParentFile().mkdirs();
-                writer = new FileWriter(file, true);
-            } catch (IOException e) {
-                throw new ExceptionInInitializerError(e);
-            }
+        final String home = System.getProperty("user.home");
+        final String fs = System.getProperty("file.separator");
+        final String filename = "log-" + new SimpleDateFormat("yyyyMMddHH").format(new Date());
+        try {
+            final File file = new File(home + fs + CONFIG_DIRECTORY + fs + filename);
+            file.getParentFile().mkdirs();
+            writer = new FileWriter(file, true);
+        } catch (IOException e) {
+            throw new ExceptionInInitializerError(e);
         }
-        log("Logger", INFO_LABEL, Version.getAsStringWithName() + " started at " + StringUtils.format(new Date()) + ".", null);
-        log("Logger", INFO_LABEL, "Log level set to " + (s != null ? s : "info") + ".", null);
+        log("Logger", LABELS[INFO], Version.getAsStringWithName() + " started at " + StringUtils.format(new Date()) + ".", null);
+        final String logLevel = System.getProperty("rvsnoop.logging");
+        if (logLevel != null) setLevel(logLevel);
+    }
+
+    public static int getLevel() {
+        return level;
     }
 
     public static Logger getLogger(Class clazz) {
         return new Logger(clazz);
     }
-    
+
     private static synchronized void log(String simpleName, String level, String message, Throwable throwable) {
+        if (Logger.level == OFF) return;
         date.setTime(System.currentTimeMillis());
         buffer.append(StringUtils.format(date)).append(" ").append(level);
         buffer.append(level.length() == 4 ? "  " : " ");
@@ -125,11 +94,11 @@ public final class Logger {
         if (throwable != null) throwable.printStackTrace(new PrintWriter(writer));
         buffer.setLength(0);
     }
-    
+
     public static void setRunningHeadless(boolean isRunningHeadless) {
         Logger.isRunningHeadless = isRunningHeadless;
     }
-    
+
     private final String simpleName;
 
     private Logger(Class clazz) {
@@ -141,17 +110,17 @@ public final class Logger {
 
     public synchronized void debug(String message) {
         if (isDebugEnabled())
-            log(simpleName, DEBUG_LABEL, message, null);
+            log(simpleName, LABELS[DEBUG], message, null);
     }
 
     public synchronized void debug(String message, Throwable throwable) {
         if (isDebugEnabled())
-            log(simpleName, DEBUG_LABEL, message, throwable);
+            log(simpleName, LABELS[DEBUG], message, throwable);
     }
 
     public synchronized void error(String message) {
         if (isErrorEnabled())
-            log(simpleName, ERROR_LABEL, message, null);
+            log(simpleName, LABELS[ERROR], message, null);
         if (!isRunningHeadless) {
             UIManager.INSTANCE.setStatusBarWarning(message);
             UIUtils.showError(message, null);
@@ -160,7 +129,7 @@ public final class Logger {
 
     public synchronized void error(String message, Throwable throwable) {
         if (isErrorEnabled())
-            log(simpleName, ERROR_LABEL, message, throwable);
+            log(simpleName, LABELS[ERROR], message, throwable);
         if (!isRunningHeadless) {
             UIManager.INSTANCE.setStatusBarWarning(message);
             UIUtils.showError(message, throwable);
@@ -169,56 +138,73 @@ public final class Logger {
 
     public synchronized void fatal(String message) {
         if (isFatalEnabled())
-            log(simpleName, FATAL_LABEL, message, null);
+            log(simpleName, LABELS[FATAL], message, null);
     }
-    
+
     public synchronized void fatal(String message, Throwable throwable) {
         if (isFatalEnabled())
-            log(simpleName, FATAL_LABEL, message, throwable);
+            log(simpleName, LABELS[FATAL], message, throwable);
     }
-    
+
     public synchronized void info(String message) {
         if (isInfoEnabled())
-            log(simpleName, INFO_LABEL, message, null);
+            log(simpleName, LABELS[INFO], message, null);
         if (!isRunningHeadless)
             UIManager.INSTANCE.setStatusBarMessage(message);
     }
-    
+
     public synchronized void info(String message, Throwable throwable) {
         if (isInfoEnabled())
-            log(simpleName, INFO_LABEL, message, throwable);
+            log(simpleName, LABELS[INFO], message, throwable);
         if (!isRunningHeadless)
             UIManager.INSTANCE.setStatusBarMessage(message);
     }
-    
+
     public static boolean isDebugEnabled() {
         return level >= DEBUG;
     }
-    
+
     public static boolean isErrorEnabled() {
         return level >= ERROR;
     }
-    
+
     private static boolean isFatalEnabled() {
         return level >= FATAL;
     }
-    
+
     public static boolean isInfoEnabled() {
         return level >= INFO;
     }
-    
+
     public static boolean isWarnEnabled() {
         return level >= WARN;
     }
-    
+
+    public static void setLevel(int level) {
+        if (level < 0 || level >= LABELS.length) throw new IndexOutOfBoundsException();
+        log("Logger", LABELS[INFO], "Setting log level to " + LABELS[level] + ".", null);
+        Logger.level = level;
+    }
+
+    public static void setLevel(String level) {
+        for (int i = 0, imax = LABELS.length; i < imax; ++i) {
+            if (LABELS[i].equalsIgnoreCase(level)) {
+                setLevel(i);
+                return;
+            }
+        }
+        if (isErrorEnabled())
+            log("Logger", LABELS[ERROR], "Not a valid log level: " + level + ".", null);
+    }
+
     public synchronized void warn(String message) {
-        log(simpleName, WARN_LABEL, message, null);
+        log(simpleName, LABELS[WARN], message, null);
         if (!isRunningHeadless)
             UIManager.INSTANCE.setStatusBarWarning(message);
     }
-    
+
     public synchronized void warn(String message, Throwable throwable) {
-        log(simpleName, WARN_LABEL, message, throwable);
+        log(simpleName, LABELS[WARN], message, throwable);
         if (!isRunningHeadless)
             UIManager.INSTANCE.setStatusBarWarning(message);
     }
