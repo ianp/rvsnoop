9c09701ecee70084e1c4df6ae1aac9204f66b693
Ian Phillips <ianp@ianp.org>
2006-03-21 14:42:13 +0000

    Text and icon values are now set once in the constructor instead of being generated dynamically each time they are needed.

--- src/rvsnoop/RvFieldTreeNode.java
+++ src/rvsnoop/RvFieldTreeNode.java
@@ -6,18 +6,6 @@
 //:CVSID:   $Id$
 package rvsnoop;
 
-import com.tibco.tibrv.TibrvException;
-import com.tibco.tibrv.TibrvIPAddr;
-import com.tibco.tibrv.TibrvIPPort;
-import com.tibco.tibrv.TibrvMsg;
-import com.tibco.tibrv.TibrvMsgField;
-import com.tibco.tibrv.TibrvXml;
-import nu.xom.Builder;
-import nu.xom.Document;
-import rvsnoop.ui.Icons;
-
-import javax.swing.Icon;
-import javax.swing.tree.TreeNode;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -26,6 +14,20 @@ import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
+import javax.swing.Icon;
+import javax.swing.tree.TreeNode;
+
+import nu.xom.Builder;
+import nu.xom.Document;
+import rvsnoop.ui.Icons;
+
+import com.tibco.tibrv.TibrvException;
+import com.tibco.tibrv.TibrvIPAddr;
+import com.tibco.tibrv.TibrvIPPort;
+import com.tibco.tibrv.TibrvMsg;
+import com.tibco.tibrv.TibrvMsgField;
+import com.tibco.tibrv.TibrvXml;
 
 /**
  * A {@link TreeNode} that wraps a Rendezvous message field.
@@ -36,19 +38,119 @@ import java.util.List;
  */
 final class RvFieldTreeNode extends LazyTreeNode {
 
+    private static Icon setIcon(TibrvMsgField field) {
+        switch (field.type) {
+            case TibrvMsg.ENCRYPTED:
+                return Icons.RV_FIELD;
+            case TibrvMsg.F32ARRAY:
+                return Icons.RV_FIELD;
+            case TibrvMsg.F64ARRAY:
+                return Icons.RV_FIELD;
+            case TibrvMsg.I8ARRAY:
+                return Icons.RV_FIELD;
+            case TibrvMsg.I16ARRAY:
+                return Icons.RV_FIELD;
+            case TibrvMsg.I32ARRAY:
+                return Icons.RV_FIELD;
+            case TibrvMsg.I64ARRAY:
+                return Icons.RV_FIELD;
+            case TibrvMsg.IPADDR32:
+                return Icons.RV_FIELD;
+            case TibrvMsg.IPPORT16:
+                return Icons.RV_FIELD;
+            case TibrvMsg.MSG:
+                return Icons.RV_MESSAGE;
+            case TibrvMsg.OPAQUE:
+                return Icons.RV_FIELD;
+            default:
+                return Icons.RV_FIELD;
+        }
+    }
+
+    private static String setText(TibrvMsgField field) {
+        final StringBuffer buffer = new StringBuffer();
+        buffer.append(field.name).append(" = ");
+        final short type = field.type;
+        if (type >= TibrvMsg.USER_FIRST && type <= TibrvMsg.USER_LAST)
+            return buffer.append(((byte[]) field.data).length).append(" bytes of custom user data.").toString();
+        switch (type) {
+            case TibrvMsg.ENCRYPTED:
+                return buffer.append(((byte[]) field.data).length).append(" bytes of encrypted data.").toString();
+            case TibrvMsg.F32ARRAY:
+                return buffer.append(ARRAY_OF).append(((float[]) field.data).length).append(" single precision floating point values.").toString();
+            case TibrvMsg.F64ARRAY:
+                return buffer.append(ARRAY_OF).append(((double[]) field.data).length).append(" double precision floating point values.").toString();
+            case TibrvMsg.I8ARRAY:
+                return buffer.append(ARRAY_OF).append(((int[]) field.data).length).append(" 8-bit signed integer values.").toString();
+            case TibrvMsg.I16ARRAY:
+                return buffer.append(ARRAY_OF).append(((long[]) field.data).length).append(" 16-bit signed integer values.").toString();
+            case TibrvMsg.I32ARRAY:
+                return buffer.append(ARRAY_OF).append(((int[]) field.data).length).append(" 32-bit signed integer values.").toString();
+            case TibrvMsg.I64ARRAY:
+                return buffer.append(ARRAY_OF).append(((long[]) field.data).length).append(" 64-bit signed integer values.").toString();
+            case TibrvMsg.U8ARRAY:
+                return buffer.append(ARRAY_OF).append(((int[]) field.data).length).append(" 8-bit unsigned integer values.").toString();
+            case TibrvMsg.U16ARRAY:
+                return buffer.append(ARRAY_OF).append(((long[]) field.data).length).append(" 16-bit unsigned integer values.").toString();
+            case TibrvMsg.U32ARRAY:
+                return buffer.append(ARRAY_OF).append(((int[]) field.data).length).append(" 32-bit unsigned integer values.").toString();
+            case TibrvMsg.U64ARRAY:
+                return buffer.append(ARRAY_OF).append(((long[]) field.data).length).append(" 64-bit unsigned integer values.").toString();
+            case TibrvMsg.IPADDR32:
+                return buffer.append("IP Address: ").append(((TibrvIPAddr) field.data).getAsString()).toString();
+            case TibrvMsg.IPPORT16:
+                return buffer.append("IP Port: ").append(Integer.toString(((TibrvIPPort) field.data).getPort())).toString();
+            case TibrvMsg.MSG:
+                return buffer.append("Nested Message").toString();
+            case TibrvMsg.OPAQUE:
+                return buffer.append(((byte[]) field.data).length).append(" bytes of opaque data.").toString();
+            case TibrvMsg.XML:
+                return buffer.append(((TibrvXml) field.data).getBytes().length).append(" bytes of compressed XML data.").toString();
+            case TibrvMsg.STRING:
+                final String s = (String) field.data;
+                if (!s.startsWith("<?xml ")) return buffer.append(s).toString();
+                return buffer.append(s.length()).append(" characters of XML data (as a string).").toString();
+            default:
+                return buffer.append(field.data).toString();
+        }
+    }
+
     private static String ARRAY_OF = "Array of ";
-    
+
+    private final boolean allowsChildren;
+
+    private final Icon icon;
+
+    private final String text;
+
     private static final Logger logger = Logger.getLogger(RvFieldTreeNode.class);
-    
+
     private final TibrvMsgField field;
+
+    /**
+     * @param parent The parent of this node.
+     * @param field The field represented by this node.
+     * @param text A custom label to use when displaying this field.
+     * @param icon A custom icon to use when displaying this field.
+     */
+    public RvFieldTreeNode(TreeNode parent, TibrvMsgField field, String text, Icon icon) {
+        super(parent);
+        this.icon = icon != null ? icon : setIcon(field);
+        this.text = text != null ? text : setText(field);
+        this.field = field;
+        if (field.type == TibrvMsg.MSG || field.type == TibrvMsg.XML) {
+            allowsChildren = true;
+        } else {
+            allowsChildren = field.type == TibrvMsg.STRING && ((String) field.data).startsWith("<?xml ");
+        }
+    }
 
     /**
      * @param parent The parent of this node.
      * @param field The field represented by this node.
      */
     public RvFieldTreeNode(TreeNode parent, TibrvMsgField field) {
-        super(parent);
-        this.field = field;
+        this(parent, field, null, null);
     }
 
     protected List createChildren() {
@@ -66,7 +168,8 @@ final class RvFieldTreeNode extends LazyTreeNode {
                 for (int i = 0; i < numChildren; ++i)
                     children.add(new RvFieldTreeNode(this, msg.getFieldByIndex(i)));
             } catch (TibrvException e) {
-                logger.error("Error reading field.", e);
+                if (Logger.isErrorEnabled())
+                    logger.error("Error reading field.", e);
             }
         }
         return children != null ? children : Collections.EMPTY_LIST;
@@ -93,72 +196,18 @@ final class RvFieldTreeNode extends LazyTreeNode {
      * @see javax.swing.tree.TreeNode#getAllowsChildren()
      */
     public boolean getAllowsChildren() {
-        return field.type == TibrvMsg.MSG || field.type == TibrvMsg.XML;
-    }
-    
-    public Icon getIcon() {
-        final StringBuffer buffer = new StringBuffer();
-        buffer.append(field.name).append(" = ");
-        switch (field.type) {
-        case TibrvMsg.ENCRYPTED:
-            return Icons.RV_FIELD;
-        case TibrvMsg.F32ARRAY:
-            return Icons.RV_FIELD;
-        case TibrvMsg.F64ARRAY:
-            return Icons.RV_FIELD;
-        case TibrvMsg.I8ARRAY:
-            return Icons.RV_FIELD;
-        case TibrvMsg.I16ARRAY:
-            return Icons.RV_FIELD;
-        case TibrvMsg.I32ARRAY:
-            return Icons.RV_FIELD;
-        case TibrvMsg.I64ARRAY:
-            return Icons.RV_FIELD;
-        case TibrvMsg.IPADDR32:
-            return Icons.RV_FIELD;
-        case TibrvMsg.IPPORT16:
-            return Icons.RV_FIELD;
-        case TibrvMsg.MSG:
-            return Icons.RV_MESSAGE;
-        case TibrvMsg.OPAQUE:
-            return Icons.RV_FIELD;
-        default:
-            return Icons.RV_FIELD;
-        }
-    }
-    
-    public String getText() {
-        final StringBuffer buffer = new StringBuffer();
-        buffer.append(field.name).append(" = ");
-        final short type = field.type;
-        if (type >= TibrvMsg.USER_FIRST && type <= TibrvMsg.USER_LAST)
-            return buffer.append(((byte[]) field.data).length).append(" bytes of custom user data.").toString();
-        switch (type) {
-        case TibrvMsg.ENCRYPTED:
-            return buffer.append(((byte[]) field.data).length).append(" bytes of encrypted data.").toString();
-        case TibrvMsg.F32ARRAY:
-            return buffer.append(ARRAY_OF).append(((float[]) field.data).length).append(" single precision floating point values.").toString();
-        case TibrvMsg.F64ARRAY:
-            return buffer.append(ARRAY_OF).append(((double[]) field.data).length).append(" double precision floating point values.").toString();
-        case TibrvMsg.I8ARRAY:
-            return buffer.append(ARRAY_OF).append(((int[]) field.data).length).append(" 8-bit integer values.").toString();
-        case TibrvMsg.I16ARRAY:
-            return buffer.append(ARRAY_OF).append(((long[]) field.data).length).append(" 16-bit integer values.").toString();
-        case TibrvMsg.I32ARRAY:
-            return buffer.append(ARRAY_OF).append(((int[]) field.data).length).append(" 32-bit integer values.").toString();
-        case TibrvMsg.I64ARRAY:
-            return buffer.append(ARRAY_OF).append(((long[]) field.data).length).append(" 64-bit integer values.").toString();
-        case TibrvMsg.IPADDR32:
-            return buffer.append("IP Address: ").append(((TibrvIPAddr) field.data).getAsString()).toString();
-        case TibrvMsg.IPPORT16:
-            return buffer.append("IP Port: ").append(Integer.toString(((TibrvIPPort) field.data).getPort())).toString();
-        case TibrvMsg.MSG:
-            return buffer.append("Nested Message").toString();
-        case TibrvMsg.OPAQUE:
-            return buffer.append(((byte[]) field.data).length).append(" bytes of opaque data.").toString();
-        default:
-            return buffer.append(field.data).toString();
-        }
+        return allowsChildren;
     }
 
+    public Icon getIcon() {
+        return icon;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getTooltip() {
+        return TibrvMsg.getTypeName(field.type);
+    }
 }
