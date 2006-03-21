38a81976ab650f1e0c43ee63130c55bf75433501
Ian Phillips <ianp@ianp.org>
2006-03-21 10:50:16 +0000

    Displays wire size instead of number of fields. Uses AE view correctly when classes are present. Displays field type as tooltip in tree view. Reformatted imports.

--- src/rvsnoop/ui/RvDetailsPanel.java
+++ src/rvsnoop/ui/RvDetailsPanel.java
@@ -6,15 +6,11 @@
 //:CVSID:   $Id$
 package rvsnoop.ui;
 
-import com.jgoodies.forms.builder.DefaultFormBuilder;
-import com.jgoodies.forms.layout.CellConstraints;
-import com.jgoodies.forms.layout.FormLayout;
-import com.tibco.tibrv.TibrvCmMsg;
-import com.tibco.tibrv.TibrvException;
-import com.tibco.tibrv.TibrvMsg;
-import rvsnoop.LazyTreeNode;
-import rvsnoop.Logger;
-import rvsnoop.RvMessageTreeNode;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.lang.reflect.Constructor;
+import java.text.NumberFormat;
 
 import javax.swing.BorderFactory;
 import javax.swing.Icon;
@@ -23,14 +19,22 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JTree;
+import javax.swing.ToolTipManager;
 import javax.swing.border.Border;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreeNode;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Font;
-import java.lang.reflect.Constructor;
+
+import rvsnoop.LazyTreeNode;
+import rvsnoop.Logger;
+import rvsnoop.RvMessageTreeNode;
+
+import com.jgoodies.forms.builder.DefaultFormBuilder;
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
+import com.tibco.tibrv.TibrvCmMsg;
+import com.tibco.tibrv.TibrvException;
+import com.tibco.tibrv.TibrvMsg;
 
 /**
  * A panel that can show details about a Rendezvous message.
@@ -40,14 +44,17 @@ import java.lang.reflect.Constructor;
  * @since 1.5
  */
 public final class RvDetailsPanel extends JPanel {
-    
+
     private static final Constructor aeMsgTreeNode;
-    
+
     private static final Logger logger = Logger.getLogger(RvDetailsPanel.class);
-    
+
+    private static final NumberFormat INT_FORMAT = NumberFormat.getIntegerInstance();
+
     static {
         Constructor aeMsgTreeNodeConstructor = null;
         try {
+            Class.forName("com.tibco.sdk.MTree");
             Class aeMsgTreeNodeClass = Class.forName("rvsnoop.AeMsgTreeNode");
             aeMsgTreeNodeConstructor = aeMsgTreeNodeClass.getConstructor(new Class[] { TibrvMsg.class });
             if (Logger.isInfoEnabled())
@@ -61,44 +68,43 @@ public final class RvDetailsPanel extends JPanel {
     }
 
     private static final long serialVersionUID = -4899980613877741151L;
-    
+
     private final JTextField cmSender = new JTextField();
-    
+
     private final JTextField cmSequence = new JTextField();
 
     private final TreeNode emptyRoot = new DefaultMutableTreeNode("[Nothing Selected]", false);
 
     private final Font headerFont = getFont().deriveFont(getFont().getSize() - 2.0f);
 
-    private Icon icon;
-    
     private final JLabel iconLabel = new JLabel();
 
     private final DefaultTreeModel model = new DefaultTreeModel(emptyRoot);
 
-    private final JTextField numFields = new JTextField();
-    
     private final JTextField replySubject = new JTextField();
 
     private final JTextField sendSubject = new JTextField();
-    
+
     private final JTextField stringEncoding = new JTextField();
 
     private final JTree tree = new JTree(model);
-    
+
+    private final JTextField wireSize = new JTextField();
+
     public RvDetailsPanel() {
         super(new BorderLayout());
         setBorder(BorderFactory.createMatteBorder(1, 1, 0, 0, Color.GRAY));
         add(createHeader(), BorderLayout.NORTH);
         add(createBody(), BorderLayout.CENTER);
         final JTextField[] fields = new JTextField[] {
-            cmSender, cmSequence, numFields, replySubject, sendSubject, stringEncoding
+            cmSender, cmSequence, wireSize, replySubject, sendSubject, stringEncoding
         };
         for (int i = 0, imax = fields.length; i < imax; ++i)
             fields[i].setEditable(false);
         clearPanel();
+        ToolTipManager.sharedInstance().registerComponent(tree);
     }
-    
+
     private void addNamedField(String name, JTextField field, int x, int y, int w, DefaultFormBuilder builder, CellConstraints cc, Border textBorder) {
         final JLabel label = builder.addLabel(name, cc.xy (x, y));
         label.setFont(headerFont);
@@ -107,17 +113,17 @@ public final class RvDetailsPanel extends JPanel {
         field.setFont(headerFont);
         field.setBorder(textBorder);
     }
-    
+
     private void clearPanel() {
         final JTextField[] fields = new JTextField[] {
-            cmSender, cmSequence, numFields, replySubject, sendSubject, stringEncoding
+            cmSender, cmSequence, wireSize, replySubject, sendSubject, stringEncoding
         };
         for (int i = 0, imax = fields.length; i < imax; ++i)
             fields[i].setText("");
         iconLabel.setIcon(Banners.MESSAGE_NONE);
         model.setRoot(emptyRoot);
     }
-    
+
     private JScrollPane createBody() {
         tree.setBorder(BorderFactory.createEmptyBorder());
         tree.setRootVisible(true);
@@ -137,7 +143,7 @@ public final class RvDetailsPanel extends JPanel {
         final Border textBorder = BorderFactory.createMatteBorder(1, 1, 1, 1, Color.LIGHT_GRAY);
         addNamedField("Send Subject",    sendSubject,    1, 1, 5, builder, cc, textBorder);
         addNamedField("Reply Subject",   replySubject,   1, 3, 5, builder, cc, textBorder);
-        addNamedField("Num. Fields",     numFields,      1, 5, 1, builder, cc, textBorder);
+        addNamedField("Wire Size",       wireSize,       1, 5, 1, builder, cc, textBorder);
         addNamedField("String Encoding", stringEncoding, 5, 5, 1, builder, cc, textBorder);
         addNamedField("CM Sender",       cmSender,       1, 7, 1, builder, cc, textBorder);
         addNamedField("CM Sequence",     cmSequence,     5, 7, 1, builder, cc, textBorder);
@@ -149,28 +155,33 @@ public final class RvDetailsPanel extends JPanel {
         header.add(iconLabel, BorderLayout.EAST);
         return header;
     }
-    
+
     public void setMessage(TibrvMsg message) {
         if (message == null) {
             clearPanel();
             return;
         }
-        numFields.setText(Integer.toString(message.getNumFields()));
+        try {
+            wireSize.setText(INT_FORMAT.format(message.getAsBytes().length) + " bytes");
+        } catch (TibrvException e) {
+            wireSize.setText("Unable to determine wire size.");
+        }
         replySubject.setText(message.getReplySubject());
         sendSubject.setText(message.getSendSubject());
         stringEncoding.setText(message.getMsgStringEncoding());
-        setCertifiedFields(message);
-        if (icon == null)
-            if (message.getSendSubject().startsWith("_RVFT."))
-                icon = Banners.MESSAGE_FT;
-            else if (message.getSendSubject().startsWith("_RV."))
-                icon = Banners.MESSAGE_ADVISORY;
-            else
-                icon = Banners.MESSAGE;
-        iconLabel.setIcon(icon);
+        if (setCertifiedFields(message)) {
+            setIconAndTooltip(Banners.MESSAGE_CM, "Certified messaging co-ordination message");
+        } else if (message.getSendSubject().startsWith("_RVFT.")) {
+            setIconAndTooltip(Banners.MESSAGE_FT, "Fault tolerance co-ordination message");
+        } else if (message.getSendSubject().startsWith("_RV.")) {
+            setIconAndTooltip(Banners.MESSAGE_ADVISORY, "Rendezvous advisory message");
+        } else {
+            setIconAndTooltip(Banners.MESSAGE, "");
+        }
         if (aeMsgTreeNode == null) {
             model.setRoot(new RvMessageTreeNode(message));
         } else {
+            if (Logger.isDebugEnabled()) logger.debug("Trying to create AE details tree from message.");
             try {
                 // We need to do this reflectively in case the SDK isn't available.
                 model.setRoot((TreeNode) aeMsgTreeNode.newInstance(new Object[] { message }));
@@ -180,14 +191,14 @@ public final class RvDetailsPanel extends JPanel {
         }
     }
 
-    private void setCertifiedFields(TibrvMsg message) {
+    private boolean setCertifiedFields(TibrvMsg message) {
         try {
-            final String sender = TibrvCmMsg.getSender(message); 
+            final String sender = TibrvCmMsg.getSender(message);
             if (sender != null && sender.length() > 0) {
                 cmSender.setText(sender);
                 cmSequence.setText(Long.toString(TibrvCmMsg.getSequence(message)));
-                icon = Banners.MESSAGE_CM;
                 cmSender.getParent().validate();
+                return true;
             } else {
                 cmSender.setText("");
                 cmSequence.setText("");
@@ -196,6 +207,11 @@ public final class RvDetailsPanel extends JPanel {
             cmSender.setText("");
             cmSequence.setText("");
         }
+        return false;
     }
 
+    private void setIconAndTooltip(Icon icon, String tip) {
+        iconLabel.setIcon(icon);
+        iconLabel.setToolTipText(tip);
+    }
 }
